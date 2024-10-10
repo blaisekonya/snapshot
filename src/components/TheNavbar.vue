12a79d95d23d304167558dc0908f3162dd09f139
blaisekonya <blaisekonya@gmail.com>
2024-10-10 03:22:12 +0200

    remove new banner

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -18,25 +18,13 @@ watch(
     <div class="px-2 sm:px-3">
       <div class="flex items-center py-2 sm:py-[12px]">
         <div class="flex flex-auto items-center">
-          <BaseButtonRound
-            v-if="!domain"
-            class="sm:hidden border-none"
-            @click="showSidebar = !showSidebar"
-          >
+          <BaseButtonRound v-if="!domain" class="sm:hidden border-none" @click="showSidebar = !showSidebar">
             <i-ho-menu class="text-skin-link" />
           </BaseButtonRound>
-          <router-link
-            :to="{ name: 'home' }"
-            class="items-center font-extrabold text-skin-primary ml-2 text-lg"
-          >
+          <router-link :to="{ name: 'home' }" class="items-center font-extrabold text-skin-primary ml-2 text-lg">
             World Association
           </router-link>
-          <span
-            v-if="env === 'demo'"
-            class="ml-1 hidden uppercase text-[12px] sm:block mb-1"
-            v-text="'testnet'"
-          />
-          <Banner class="ml-3" />
+          <span v-if="env === 'demo'" class="ml-1 hidden uppercase text-[12px] sm:block mb-1" v-text="'testnet'" />
         </div>
         <div :key="web3Account" class="flex sm:space-x-2">
           <div class="hidden sm:block">
@@ -50,18 +38,14 @@ watch(
       </div>
     </div>
   </div>
-  <div
-    v-if="pendingTransactions.length > 0"
-    class="flex items-center justify-center gap-x-2 bg-skin-border py-2 text-center text-skin-link"
-    :class="{
+  <div v-if="pendingTransactions.length > 0"
+    class="flex items-center justify-center gap-x-2 bg-skin-border py-2 text-center text-skin-link" :class="{
       'cursor-pointer': pendingTransactions.length > 0
-    }"
-    @click="
+    }" @click="
       pendingTransactionsWithHash.length
         ? (showPendingTransactionsModal = true)
         : null
-    "
-  >
+      ">
     <TuneLoadingSpinner class="text-skin-link" />
     <span>
       {{ $t('setup.pendingTransactions') }}:
@@ -69,9 +53,6 @@ watch(
     </span>
   </div>
   <Teleport to="#modal">
-    <ModalPendingTransactions
-      :open="showPendingTransactionsModal"
-      @close="showPendingTransactionsModal = false"
-    />
+    <ModalPendingTransactions :open="showPendingTransactionsModal" @close="showPendingTransactionsModal = false" />
   </Teleport>
 </template>
